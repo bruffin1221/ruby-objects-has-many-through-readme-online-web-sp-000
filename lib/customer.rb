8497b586ffd720be5ecdc8a88c53f43e61e78768
Bruce Ruffin <bruffin1221@gmail.com>
2019-12-04 03:11:19 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -2,13 +2,19 @@ class Customer
 
 attr_accessor :name, :age
 
-def initialie(name, age)
-  @name=name
-  @age=age
+@@all=[]
+
+def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
 end
-
-@@all
-
 
 
 
